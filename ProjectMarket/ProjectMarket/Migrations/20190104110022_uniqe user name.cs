d1b13106f31e41899ee4989eb8db0023789541e1
reshefya <reshefyaron@gmail.com>
2019-01-04 13:04:19 +0200

    user name is now uniqe

---
+++ ProjectMarket/ProjectMarket/Migrations/20190104110022_uniqe user name.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectMarket.Migrations
+{
+    public partial class uniqeusername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_User_UserName",
+                table: "User",
+                column: "UserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_User_UserName",
+                table: "User");
+        }
+    }
+}

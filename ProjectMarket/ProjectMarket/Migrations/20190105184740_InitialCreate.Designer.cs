74ea7d6d8db43df698822ef5a9da524d9d14b812
User <User@WINDEV1811EVAL>
2019-01-05 12:33:19 -0800

    - Added google map - Change Project model

--- ProjectMarket/ProjectMarket/Migrations/20190105184740_InitialCreate.Designer.cs
+++ ProjectMarket/ProjectMarket/Migrations/20190105184740_InitialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectMarket.Models;
 
 namespace ProjectMarket.Migrations
 {
     [DbContext(typeof(ProjectMarketContext))]
-    partial class ProjectMarketContextModelSnapshot : ModelSnapshot
+    [Migration("20190105184740_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -92,17 +94,11 @@ namespace ProjectMarket.Migrations
 
                     b.Property<int>("FieldOfStudyId");
 
-                    b.Property<double>("LocationLatitude");
-
-                    b.Property<double>("LocationLongitude");
-
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasMaxLength(50);
 
                     b.Property<int>("OwnerId");
-
-                    b.Property<int>("Price");
 
                     b.HasKey("Id");
 
---
+++ ProjectMarket/ProjectMarket/Migrations/20190105201956_Change project.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectMarket.Migrations
+{
+    public partial class Changeproject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "LocationLatitude",
+                table: "Project",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "LocationLongitude",
+                table: "Project",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Project",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationLatitude",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "LocationLongitude",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Project");
+        }
+    }
+}

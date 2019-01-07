ac61de48f4c2185d764710d3e6c6b153e5f2d509
reshefya <reshefyaron@gmail.com>
2019-01-07 23:08:07 +0200

    Breezer is good for coding

---
+++ ProjectMarket/ProjectMarket/Migrations/20190107210042_grades_and_ranks.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectMarket.Migrations
+{
+    public partial class grades_and_ranks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sale_Project_ProjectId",
+                table: "Sale");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "Sale",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Grade",
+                table: "Sale",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sale_Project_ProjectId",
+                table: "Sale",
+                column: "ProjectId",
+                principalTable: "Project",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sale_Project_ProjectId",
+                table: "Sale");
+
+            migrationBuilder.DropColumn(
+                name: "Grade",
+                table: "Sale");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "Sale",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sale_Project_ProjectId",
+                table: "Sale",
+                column: "ProjectId",
+                principalTable: "Project",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ ProjectMarket/ProjectMarket/ViewModels/ProjectInStoreView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectMarket.ViewModels
+{
+    public class ProjectInStoreView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string OwnerName { get; set; }
+        public double? AvgGrade { get; set; }
+        public double? Rank { get; set; }
+    }
+}

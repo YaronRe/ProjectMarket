d7dca52724811138294bcbf60d7a288952202138
Netanel <bitanetanel@gmail.com>
2019-01-22 22:22:53 +0200

    Machine learning Bi**es!!

--- ProjectMarket/ProjectMarket/Data/ProjectMarketContext.cs
+++ ProjectMarket/ProjectMarket/Data/ProjectMarketContext.cs
@@ -29,7 +29,7 @@ namespace ProjectMarket.Models
                 new User() { Id = 3, EMail = "user@gmail.com", FirstName = "UserF", LastName = "UserL", IsAdmin = false, UserName = "Buyer", Password = "12345678" },
                 new User() { Id = 4, EMail = "user@gmail.com", FirstName = "UserF", LastName = "UserL", IsAdmin = false, UserName = "Seller", Password = "12345678" },
                 new User() { Id = 5, EMail = "user@gmail.com", FirstName = "UserF", LastName = "UserL", IsAdmin = false, UserName = "BuyerSeller", Password = "12345678" },
-                new User() { Id = 6, EMail = "user@gmail.com", FirstName = "UserF", LastName = "UserL", IsAdmin = false, UserName = "deleted", Password = "12345678" ,IsDeleted = true},
+                new User() { Id = 6, EMail = "user@gmail.com", FirstName = "UserF", LastName = "UserL", IsAdmin = false, UserName = "deleted", Password = "12345678", IsDeleted = true },
                 new User() { Id = 7, EMail = "user@gmail.com", FirstName = "UserF", LastName = "UserL", IsAdmin = false, UserName = "Grader", Password = "12345678" }
                 );
             modelBuilder.Entity<AcademicInstitute>().HasData(
@@ -42,22 +42,33 @@ namespace ProjectMarket.Models
                 new FieldOfStudy() { Id = 3, Name = "פיזיקה" }
             );
             modelBuilder.Entity<Project>().HasData(
-                new Project() {Id = 1,Name = "DeletedOfUser" , OwnerId = 2,Description="",AcademicInstituteId=1,FieldOfStudyId = 2,IsDeleted=true,Price=10.0},
-                new Project() {Id = 2,Name = "DeletedOfDelUser" , OwnerId = 6,Description="",AcademicInstituteId=1,FieldOfStudyId = 2,IsDeleted=true,Price=20.0 },
-                new Project() {Id = 3,Name = "Sold" , OwnerId = 5,Description="",AcademicInstituteId=1,FieldOfStudyId = 2,IsDeleted=false,Price=30.0 },
-                new Project() {Id = 4,Name = "NotSold" , OwnerId = 4,Description="",AcademicInstituteId=1,FieldOfStudyId = 2,IsDeleted=false,Price=40.0 },
-                new Project() {Id = 5,Name = "SoldAndDeleted" , OwnerId = 4,Description="",AcademicInstituteId=1,FieldOfStudyId = 2,IsDeleted=true,Price=50.0 },
-                new Project() {Id = 6,Name = "SoldMultiple" , OwnerId = 4,Description="",AcademicInstituteId=1,FieldOfStudyId = 2,IsDeleted=false,Price=60.0 },
-                new Project() {Id = 7,Name = "Graded" , OwnerId = 4,Description="",AcademicInstituteId=1,FieldOfStudyId = 2,IsDeleted=false,Price=60.0 },
-                new Project() {Id = 8,Name = "NotGraded" , OwnerId = 4,Description="",AcademicInstituteId=1,FieldOfStudyId = 2,IsDeleted=false,Price=60.0 }
+                new Project() { Id = 1, Name = "DeletedOfUser", OwnerId = 2, Description = "", AcademicInstituteId = 1, FieldOfStudyId = 2, IsDeleted = true, Price = 10.0 },
+                new Project() { Id = 2, Name = "DeletedOfDelUser", OwnerId = 6, Description = "", AcademicInstituteId = 1, FieldOfStudyId = 2, IsDeleted = true, Price = 20.0 },
+                new Project() { Id = 3, Name = "Sold", OwnerId = 5, Description = "", AcademicInstituteId = 1, FieldOfStudyId = 2, IsDeleted = false, Price = 30.0 },
+                new Project() { Id = 4, Name = "NotSold", OwnerId = 4, Description = "", AcademicInstituteId = 1, FieldOfStudyId = 2, IsDeleted = false, Price = 40.0 },
+                new Project() { Id = 5, Name = "SoldAndDeleted", OwnerId = 4, Description = "", AcademicInstituteId = 1, FieldOfStudyId = 2, IsDeleted = true, Price = 50.0 },
+                new Project() { Id = 6, Name = "SoldMultiple", OwnerId = 4, Description = "", AcademicInstituteId = 1, FieldOfStudyId = 2, IsDeleted = false, Price = 60.0 },
+                new Project() { Id = 7, Name = "Graded", OwnerId = 4, Description = "", AcademicInstituteId = 1, FieldOfStudyId = 2, IsDeleted = false, Price = 60.0 },
+                new Project() { Id = 8, Name = "NotGraded", OwnerId = 4, Description = "", AcademicInstituteId = 1, FieldOfStudyId = 2, IsDeleted = false, Price = 60.0 }
                 );
             modelBuilder.Entity<Sale>().HasData(
-                new Sale() { Id= 1,ProjectId = 3,BuyerId = 1,Price = 0.0,AcademicInstituteId= null,Rank=null,Grade=null},
-                new Sale() { Id= 2,ProjectId = 5,BuyerId = 1,Price = 0.0,AcademicInstituteId= 1,Rank=2,Grade=70},
-                new Sale() { Id= 3,ProjectId = 6,BuyerId = 3,Price = 0.0,AcademicInstituteId= null,Rank=null,Grade=null},
-                new Sale() { Id= 4,ProjectId = 6,BuyerId = 5,Price = 0.0,AcademicInstituteId= 2,Rank=3,Grade=70},
-                new Sale() { Id= 5,ProjectId = 7,BuyerId = 3,Price = 0.0,AcademicInstituteId= 1,Rank=4,Grade=80},
-                new Sale() { Id= 6,ProjectId = 8,BuyerId = 3,Price = 0.0,AcademicInstituteId= null,Rank=null,Grade=null}
+                new Sale() { Id = 1,  ProjectId = 3, BuyerId = 1, Price = 0.0, AcademicInstituteId = null, Rank = null, Grade = null },
+                new Sale() { Id = 2,  ProjectId = 4, BuyerId = 1, Price = 0.0, AcademicInstituteId = null, Rank = null, Grade = null },
+                new Sale() { Id = 3,  ProjectId = 6, BuyerId = 1, Price = 0.0, AcademicInstituteId = 1, Rank = 2, Grade = 70 },
+                
+                new Sale() { Id = 5,  ProjectId = 3, BuyerId = 3, Price = 0.0, AcademicInstituteId = null, Rank = null, Grade = null },
+                new Sale() { Id = 6,  ProjectId = 5, BuyerId = 3, Price = 0.0, AcademicInstituteId = null, Rank = null, Grade = null },
+                new Sale() { Id = 4,  ProjectId = 6, BuyerId = 3, Price = 0.0, AcademicInstituteId = null, Rank = null, Grade = null },
+                new Sale() { Id = 7,  ProjectId = 7, BuyerId = 3, Price = 0.0, AcademicInstituteId = 1, Rank = 4, Grade = 80 },
+                new Sale() { Id = 8,  ProjectId = 8, BuyerId = 3, Price = 0.0, AcademicInstituteId = null, Rank = null, Grade = null },
+                
+                new Sale() { Id = 10, ProjectId = 3, BuyerId = 5, Price = 0.0, AcademicInstituteId = 2, Rank = 3, Grade = 70 },
+                new Sale() { Id = 11, ProjectId = 5, BuyerId = 5, Price = 0.0, AcademicInstituteId = 2, Rank = 3, Grade = 70 },
+                new Sale() { Id = 9, ProjectId = 6, BuyerId = 5, Price = 0.0, AcademicInstituteId = 2, Rank = 3, Grade = 70 },
+                
+                new Sale() { Id = 12, ProjectId = 3, BuyerId = 6, Price = 0.0, AcademicInstituteId = 2, Rank = 3, Grade = 70 },
+                new Sale() { Id = 13, ProjectId = 5, BuyerId = 6, Price = 0.0, AcademicInstituteId = 2, Rank = 3, Grade = 70 },
+                new Sale() { Id = 14, ProjectId = 6, BuyerId = 6, Price = 0.0, AcademicInstituteId = 2, Rank = 3, Grade = 70 }
                 );
 
         }
---
+++ ProjectMarket/ProjectMarket/Migrations/20190122201641_pasten2.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectMarket.Migrations
+{
+    public partial class pasten2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Sale",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ProjectId",
+                value: 6);
+
+            migrationBuilder.InsertData(
+                table: "Sale",
+                columns: new[] { "Id", "AcademicInstituteId", "AcceptedByBuyer", "AcceptedBySeller", "BuyerId", "Grade", "Price", "ProjectId", "Rank" },
+                values: new object[] { 14, 2, false, false, 6, 70, 0.0, 6, 3 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Sale",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.UpdateData(
+                table: "Sale",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ProjectId",
+                value: 5);
+        }
+    }
+}

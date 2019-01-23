089a97eff14599c1e734cfaa0e3d3e4924c69970
reshefya <reshefyaron@gmail.com>
2019-01-23 15:38:21 +0200

    change map location

---
+++ ProjectMarket/ProjectMarket/Migrations/20190122204245_removed-lat-and-len.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectMarket.Migrations
+{
+    public partial class removedlatandlen : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
